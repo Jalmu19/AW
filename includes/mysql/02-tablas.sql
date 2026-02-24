f72d8ad997770ce255600ea32dbb844c1846fd9d
yo262 <irodri15@ucm.es>
2026-02-24 17:53:40 +0100

    Creacion base de datos y algunas tablas

---
+++ includes/mysql/02-tablas.sql
@@ -0,0 +1,92 @@
+DROP TABLE IF EXISTS `Usuario`;
+DROP TABLE IF EXISTS `Categoria`;
+DROP TABLE IF EXISTS `Producto`;
+DROP TABLE IF EXISTS `Pedido`;
+
+
+
+CREATE TABLE IF NOT EXISTS `Usuario` (
+    `nombre` varchar(15) NOT NULL,
+    `apellidos` varchar(30)  NOT NULL,
+    `email` varchar(30)  NOT NULL,
+    `contraseña_hash` varchar(30) NOT NULL,
+    `rol` ENUM('gerente', 'camarero', 'cocinero', 'cliente') NOT NULL,
+    `nombre_usuario` varchar(30) NOT NULL,
+
+    PRIMARY KEY(`nombre_usuario`)
+
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `Pedido` (
+    `fecha_hora` date NOT NULL,
+    `num_pedido` int NOT NULL,
+    `tipo` varchar(30) NOT NULL,
+    `total` float,
+    `estado` varchar(11) NOT NULL,
+
+    `cliente` varchar(30) NOT NULL,
+    `camarero` varchar(30) NOT NULL,
+    `cocinero` varchar(30) NOT NULL,
+
+    PRIMARY KEY(`fecha_hora`, `num_pedido`),
+    KEY `num_pedido` (`num_pedido`) --para acelerar busquedas
+    FOREIGN KEY (`cliente`) REFERENCES `Usuario`(`nombre_usuario`)
+    FOREIGN KEY (`camarero`) REFERENCES `Usuario`(`nombre_usuario`)
+    FOREIGN KEY (`cocinero`) REFERENCES `Usuario`(`nombre_usuario`)
+    
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `Categoria` (
+    `nombre` varchar(15) NOT NULL,
+    `descripcion` varchar(50) NOT NULL,
+    `imagen` varchar(50) NOT NULL,
+
+    `gerente` varchar(30) NOT NULL,
+
+    PRIMARY KEY(`nombre`),
+    FOREIGN KEY (`gerente`) REFERENCES `Usuario`(`nombre_usuario`)
+    
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `Producto` (
+    `nombre` varchar(15) NOT NULL,
+    `precio` float NOT NULL,
+    `disponibilidad` boolean NOT NULL,
+    `iva` float(30) NOT NULL,
+    `ofertado` boolean NOT NULL,
+    `descripcion` varchar(50) NOT NULL,
+    `imagen` varchar(50) NOT NULL,
+
+    `categoria` varchar(30) NOT NULL,
+
+    PRIMARY KEY(`nombre`),
+    FOREIGN KEY (`categoria`) REFERENCES `Categoria`(`nombre`)
+    
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `Pedido-Producto` (
+    `nombre` varchar(15) NOT NULL,
+    `fecha_hora` date NOT NULL,
+    `num_pedido` int NOT NULL,
+
+    PRIMARY KEY(`nombre`, `fecha_hora`, `num_pedido`),
+    FOREIGN KEY (`nombre`) REFERENCES `Producto`(`nombre`)
+    FOREIGN KEY (`fecha_hora`) REFERENCES `Pedido`(`fecha_hora`)
+    FOREIGN KEY (`num_pedido`) REFERENCES `Pedido`(`num_pedido`)
+    
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `Cocinero-Producto` (
+    `cocinero` varchar(15) NOT NULL,
+    `nombre_producto`varchar(15)  int NOT NULL,
+
+    PRIMARY KEY(`cocinero`, `nombre_producto`),
+    FOREIGN KEY (`cocinero`) REFERENCES `Usuario`(`nombre_usuario`)
+    FOREIGN KEY (`nombre_producto`) REFERENCES `Producto`(`nombre`)   
+    
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;

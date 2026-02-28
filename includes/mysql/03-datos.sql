4ad15d72cf5a2e50063e1e799754df617db8d77c
yo262 <irodri15@ucm.es>
2026-02-28 11:44:55 +0100

    actualizado rutas imagenes

--- includes/mysql/03-datos.sql
+++ includes/mysql/03-datos.sql
@@ -1,10 +1,10 @@
-INSERT INTO Producto VALUES('pasta', 10.0, true, 21, true, 'macarrones con salsa boloñesa','pasta.png','hidratos de carbono');
-INSERT INTO Producto VALUES('salmorejo', 10.0, true, 21, true, 'salmorejo cordobés','salmorejo.png','verdura');
-INSERT INTO Producto VALUES('salmón', 10.0, true, 21, true, 'salmón al horno','salmon.png','pescado');
-INSERT INTO Producto VALUES('pollo', 10.0, true, 21, true, 'pollo con patatas','pollo.png','carne');
-INSERT INTO Producto VALUES('tiramisu', 10.0, true, 21, true, 'tiramisu italiano','tiramisu.png','postre');
-INSERT INTO Producto VALUES('brownie', 10.0, true, 21, true, 'brownie de chocolate','brownie.png', 'postre');
-INSERT INTO Producto VALUES('coca-cola', 2.5, true, 21, true, 'coca-cola','coca_cola.png', 'bebida');
+INSERT INTO Producto VALUES('pasta', 10.0, true, 21, true, 'macarrones con salsa boloñesa','../img/productos/pasta.png','hidratos de carbono');
+INSERT INTO Producto VALUES('salmorejo', 10.0, true, 21, true, 'salmorejo cordobés','../img/productos/salmorejo.png','verdura');
+INSERT INTO Producto VALUES('salmón', 10.0, true, 21, true, 'salmón al horno','../img/productos/salmon.png','pescado');
+INSERT INTO Producto VALUES('pollo', 10.0, true, 21, true, 'pollo con patatas','../img/productos/pollo.png','carne');
+INSERT INTO Producto VALUES('tiramisu', 10.0, true, 21, true, 'tiramisu italiano','../img/productos/tiramisu.png','postre');
+INSERT INTO Producto VALUES('brownie', 10.0, true, 21, true, 'brownie de chocolate','../img/productos/brownie.png', 'postre');
+INSERT INTO Producto VALUES('coca-cola', 2.5, true, 21, true, 'coca-cola','../img/productos/coca_cola.png', 'bebida');
 
 INSERT INTO Usuario VALUES('Marta', 'Pérez Gómez', 'marta@gmail.com', '', 'cliente', 'martita')
 INSERT INTO Usuario VALUES('Jose', 'Sánchez López', 'jose@gmail.com', '', 'cliente', 'jose')
@@ -14,12 +14,12 @@ INSERT INTO Usuario VALUES('Silvia', 'Díaz Moreno', 'silvia@gmail.com', '', 'co
 INSERT INTO Usuario VALUES('Marcos', 'Alonso Gutierrez', 'marcos@gmail.com', '', 'cocinero', 'marcos')
 INSERT INTO Usuario VALUES('Emma', 'Ortiz Gómez', 'emma@gmail.com', '', 'gerente', 'emma')
 
-INSERT INTO Categoria VALUES('hidratos de carbono', 'macronutriente esencial', 'hidratos_de_carbono.png', 'emma')
-INSERT INTO Categoria VALUES('verdura', 'fuente de vitaminas', 'verdura.png', 'emma')
-INSERT INTO Categoria VALUES('pescado', 'fuente de proteínas, omega-3 y otros', 'pescado.png', 'emma')
-INSERT INTO Categoria VALUES('carne', 'fuente de proteínas, grasas y minerales', 'carne.png', 'emma')
-INSERT INTO Categoria VALUES('postre', 'plato dulce/salado', 'postres.png', 'emma')
-INSERT INTO Categoria VALUES('bebida', 'liquido para refrescar', 'bebidas.png', 'emma')
+INSERT INTO Categoria VALUES('hidratos de carbono', 'macronutriente esencial', '../img/categorias/hidratos_de_carbono.png', 'emma')
+INSERT INTO Categoria VALUES('verdura', 'fuente de vitaminas', '../img/categorias/verdura.png', 'emma')
+INSERT INTO Categoria VALUES('pescado', 'fuente de proteínas, omega-3 y otros', '../img/categorias/pescado.png', 'emma')
+INSERT INTO Categoria VALUES('carne', 'fuente de proteínas, grasas y minerales', '../img/categorias/carne.png', 'emma')
+INSERT INTO Categoria VALUES('postre', 'plato dulce/salado', '../img/categorias/postres.png', 'emma')
+INSERT INTO Categoria VALUES('bebida', 'liquido para refrescar', '../img/categorias/bebidas.png', 'emma')
 
 INSERT INTO Pedido VALUES('12-02-2025 22:00', 1, , 22.50, 'Cocinando', 'martita', 'ana', 'silvia')
 INSERT INTO Pedido VALUES('13-02-2025 15:02', 2, , 12.25, 'Cocinando', 'martita', 'ana', 'marcos')

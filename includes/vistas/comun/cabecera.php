3cb59785ff6acf5fc7aa8f3b4a1a361fd617da8f
yo262 <irodri15@ucm.es>
2026-04-08 20:35:57 +0200

    items de la cabecera mejorados

--- includes/vistas/comun/cabecera.php
+++ includes/vistas/comun/cabecera.php
@@ -11,21 +11,33 @@ function perfil() {
 
         $rutaImg = RUTA_APP . '/img/avatares/' . $avatar;
         $fotoCarrito = RUTA_APP . '/img/carrito.png';
+        $fotoLogout = RUTA_APP . '/img/logout.png';
         $rutaPerfil = RUTA_APP . '/miCuenta.php';
         $rutaLogout = RUTA_APP . '/logout.php';
         $rutaCarrito = RUTA_APP. '/carrito.php';
 
-        $html = "<div>
-            Hola, 
-            <a href='$rutaPerfil'>$nombre<img src='$rutaImg' class = 'cabecera'></a>
-            <a href='$rutaCarrito'>Mi carrito<img src='$fotoCarrito' class = 'cabecera'></a>
-            <a href='$rutaLogout'>Salir</a>
+        $html = "<div class='perfil_carrito_salir'>
+                  
+            <a href='$rutaPerfil' class = 'perfil_cabecera'> 
+                <img src='$rutaImg' class = 'perfil'>   $nombre 
+            </a>             
+                        
+            <a href='$rutaCarrito' class = 'carrito_cabecera'> 
+                <img src='$fotoCarrito' class = 'carrito'>
+                Mi carrito 
+            </a>       
+            
+            <a href='$rutaLogout' class = 'salir_cabecera'>
+                <img src='$fotoLogout' class = 'logout'>
+                Salir
+            </a>
+
         </div>";
     } else {
         $rutaRegistro = RUTA_APP . '/registro.php';
         $rutaLogin = RUTA_APP . '/login.php';
 
-        $html = "<div class=login-registro>
+        $html = "<div>
             <a href='$rutaLogin'>Login</a> | <a href='$rutaRegistro'>Registro</a>
         </div>";
     }
@@ -38,10 +50,8 @@ function perfil() {
         <a href="<?= RUTA_APP ?>/index.php">
             <img src="<?= RUTA_APP ?>/img/logo_bistro.png" alt="Bistro FDI Logo" class="logo-central"/>
         </a>
-    </div>
-        
-    <div class="perfil">
-        <?= perfil() ?>
-    </div>
+    </div>    
+    
+    <?= perfil() ?> 
 
 </header>

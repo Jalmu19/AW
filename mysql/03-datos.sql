c44234c49e1494844caa73619b67b1112527ade8
Almudena García Navas <almude12@ucm.es>
2026-04-25 13:09:06 +0200

    Reorganizacion datos
    Para evitar errores en la importacion en phpMyAdmin

--- mysql/03-datos.sql
+++ mysql/03-datos.sql
@@ -1,14 +1,4 @@
 
-INSERT INTO Producto VALUES('croquetas', 7.0, true, 21, true, 'croquetas de jamón','croquetas.png',true,'entrante');
-INSERT INTO Producto VALUES('pasta', 10.0, true, 21, true, 'macarrones con salsa boloñesa','pasta.png',true,'primer plato');
-INSERT INTO Producto VALUES('salmorejo', 6.0, true, 21, true, 'salmorejo cordobés','salmorejo.png',true,'primer plato');
-INSERT INTO Producto VALUES('salmon', 11.0, true, 21, true, 'salmón al horno','salmon.png',true,'segundo plato');
-INSERT INTO Producto VALUES('pollo', 10.0, true, 21, true, 'pollo con patatas','pollo.png',true,'segundo plato');
-INSERT INTO Producto VALUES('tiramisu', 4.50, true, 21, true, 'tiramisu italiano','tiramisu.png',true,'postre');
-INSERT INTO Producto VALUES('brownie',4.50, true, 21, true, 'brownie de chocolate','brownie.png',true, 'postre');
-INSERT INTO Producto VALUES('coca-cola', 2.5, true, 21, true, 'coca-cola','coca_cola.png', false,'bebida');
-INSERT INTO Producto VALUES('cafe solo', 2.5, true, 21, true, 'cafe solo','cafe_solo.png', false,'bebida');
-INSERT INTO Producto VALUES('cafe con leche', 2.75, true, 21, true, 'cafe con leche','cafe_con_leche.png', false,'bebida');
 
 INSERT INTO Usuarios VALUES('martita', 'Marta', 'Pérez Gómez', 'marta@gmail.com', '$2y$10$G4jdtAR1AibJm0/PGgI7cunmySeB0vbPaDV5.G/LddxoOzZ8ySbYm', 1, 'avatar24.png');
 INSERT INTO Usuarios VALUES('jose', 'Jose', 'Sánchez López', 'jose@gmail.com', '$2y$10$G4jdtAR1AibJm0/PGgI7cunmySeB0vbPaDV5.G/LddxoOzZ8ySbYm',1,'avatar11.png');
@@ -24,6 +14,16 @@ INSERT INTO Categoria VALUES('segundo plato', 'segundos platos');
 INSERT INTO Categoria VALUES('postre', 'postres caseros');
 INSERT INTO Categoria VALUES('bebida', 'bebidas');
 
+INSERT INTO Producto VALUES('croquetas', 7.0, true, 21, true, 'croquetas de jamón','croquetas.png',true,'entrante');
+INSERT INTO Producto VALUES('pasta', 10.0, true, 21, true, 'macarrones con salsa boloñesa','pasta.png',true,'primer plato');
+INSERT INTO Producto VALUES('salmorejo', 6.0, true, 21, true, 'salmorejo cordobés','salmorejo.png',true,'primer plato');
+INSERT INTO Producto VALUES('salmon', 11.0, true, 21, true, 'salmón al horno','salmon.png',true,'segundo plato');
+INSERT INTO Producto VALUES('pollo', 10.0, true, 21, true, 'pollo con patatas','pollo.png',true,'segundo plato');
+INSERT INTO Producto VALUES('tiramisu', 4.50, true, 21, true, 'tiramisu italiano','tiramisu.png',true,'postre');
+INSERT INTO Producto VALUES('brownie',4.50, true, 21, true, 'brownie de chocolate','brownie.png',true, 'postre');
+INSERT INTO Producto VALUES('coca-cola', 2.5, true, 21, true, 'coca-cola','coca_cola.png', false,'bebida');
+INSERT INTO Producto VALUES('cafe solo', 2.5, true, 21, true, 'cafe solo','cafe_solo.png', false,'bebida');
+INSERT INTO Producto VALUES('cafe con leche', 2.75, true, 21, true, 'cafe con leche','cafe_con_leche.png', false,'bebida');
 
 INSERT INTO Pedido VALUES('2025-02-12 22:00:00', 1, 'en local', 22.50, 'Cocinando', 'martita', 'ana', 'silvia');
 INSERT INTO Pedido VALUES('2025-02-13 15:02:00', 2, 'en local', 12.25, 'Cocinando', 'martita', 'ana', 'marcos');
@@ -37,7 +37,6 @@ INSERT INTO Pedido_Producto VALUES('brownie', 1, '15-02-2025 12:24:00', 3, false
 INSERT INTO Pedido_Producto VALUES('pollo', 1, '16-02-2025 20:46:00', 4, false);
 INSERT INTO Pedido_Producto VALUES('salmon', 1, '17-02-2025 19:37:00', 5, false);
 INSERT INTO Pedido_Producto VALUES('coca-cola', 1, '17-02-2025 19:37:00', 5, false);
-
 
 INSERT INTO Oferta(`nombre`, `descripcion`, `fecha_ini`, `fecha_fin`, `descuento`) VALUES('Despertar dulce', 'Desayuna lo mejor del dia', '2026-04-18 9:00:00', '2026-12-18 9:00:00', 10.0);
 INSERT INTO Oferta(`nombre`, `descripcion`, `fecha_ini`, `fecha_fin`, `descuento`) VALUES('Puente de mayo', 'Preparate para el verano', '2026-04-20 9:00:00', '2026-05-20 9:00:00', 20.0);
